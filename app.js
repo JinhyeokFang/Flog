9bc8ce2c7283f9302ed05dac321b7c73a98c6024
jinhyeok <hyeki0207@gmail.com>
2017-05-19 20:30:02 +0900

    add app.listen and some comment

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var app = express();
 
+//templete engine
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+//middle ware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('./public'));
@@ -17,6 +19,10 @@ app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
+});
+
+app.listen(8080, () => {
+    console.log("server is running on port 8080");
 });
 
 module.exports = app;
