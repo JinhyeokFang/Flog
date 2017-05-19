ad3bde76d60b6b26db3b0ab7a51d2da1d89f50aa
jinhyeok <hyeki0207@gmail.com>
2017-05-20 07:24:21 +0900

    set router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET blog page. */
+router.get('/blog/:name', function(req, res) {
+  res.render('index', { name: req.params.name });
+});
+
+/* GET index page. */
 router.get('/', function(req, res) {
-  res.render('index', { name: 'UserName' });
+
+});
+
+/* GET signin page. */
+router.get('/signin', function(req, res) {
+
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res) {
+
+});
+
+/* POST addUser page. */
+router.post('/addUser', function(req, res) {
+
+});
+
+/* POST addContents page. */
+router.post('/addContents', function(req, res) {
+
+});
+
+/* POST addContents page. */
+router.post('/addContents', function(req, res) {
+
+});
+
+/* DELETE addContents page. */
+router.delete('/deleteContents', function(req, res) {
+
 });
 
 module.exports = router;
