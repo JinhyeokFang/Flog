d8ceddb538841b4e64e35fd61e626f14924f3897
jinhyeok <hyeki0207@gmail.com>
2017-05-20 09:12:48 +0900

    disuse mongoose

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var app = express();
 
