038ff6f5628ba66fc503ba3b5166dc7f718bbf98
jinhyeok <hyeki0207@gmail.com>
2017-05-20 10:41:33 +0900

    add login system

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+//mongodb
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test'); // 기본 설정에 따라 포트가 상이 할 수 있습니다.
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("mongo db connection OK.");
+});
+
+var UserSchema = mongoose.Schema({
+  id: String,
+  password: String
+});
+
+var UserModel = mongoose.model("UserModel", UserSchema);
+
 /* GET blog page. */
 router.get('/blog/:name', function(req, res) {
   res.render('blog', { name: req.params.name });
@@ -23,12 +39,30 @@ router.get('/signup', function(req, res) {
 
 /* POST addUser page. */
 router.post('/addUser', function(req, res) {
+  var UserIns = new UserModel({ id:req.body.id, password:req.body.pass });
+
+  UserIns.save(function(err, UserIns){
+    if(err) return console.error(err);
+    res.send({
+      id: UserIns.id,
+      password: UserIns.password
+    })
+  });
 
 });
 
 /* POST User page. */
 router.post('/User', function(req, res) {
-
+  UserModel.findOne({
+    id: req.body.id,
+    password: req.body.pass
+  }, (err,user) => {
+    if(err) return console.error(err);
+    if(!user) res.render('signFail');
+    res.send({
+      user: user
+    });
+  });
 });
 
 /* POST addContents page. */
